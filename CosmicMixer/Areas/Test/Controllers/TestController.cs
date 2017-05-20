7e3a4ca24036325582453c6a72523b2df584eb91
Davide Spano <davide.spano.x@gmail.com>
2017-05-20 11:44:44 +0100

    first commit

---
+++ CosmicMixer/Areas/Test/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CosmicMixer.Areas.Test.Controllers
+{
+    [Area("Test")]
+    public class TestController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "this is page one title";
+            return View();
+        }
+    }
+}
