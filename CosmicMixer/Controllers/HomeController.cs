972154514b5dabb61c4a3093bb0d48bb76bce7ad
Davide Spano <davide.spano.x@gmail.com>
2018-04-14 13:10:54 +0100

    changes to Index.cshtml to chain calls to the server for tiles and nex model for tiles in HomeController

--- CosmicMixer/Controllers/HomeController.cs
+++ CosmicMixer/Controllers/HomeController.cs
@@ -8,6 +8,22 @@ using CosmicMixer.Models;
 
 namespace CosmicMixer.Controllers
 {
+    public class Tile
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string UrlImage { get; private set; }
+        public string UrlArticle { get; private set; }
+
+        public Tile(int id, string title, string urlImage, string urlAritcle)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.UrlImage = urlImage;
+            this.UrlArticle = urlAritcle;
+        }
+    }
+
     public class HomeController : Controller
     {
         public IActionResult Index()
@@ -17,7 +33,18 @@ namespace CosmicMixer.Controllers
 
         public IActionResult GetTiles(int fromId)
         {
-            object data = new { Id = fromId + 1 };
+            //object data = new { Id = fromId + 1 };
+
+            // object tile1 = new { Id = fromId + 1 };
+            // object tile2 = new { Id = fromId + 2 };
+            // object tile3 = new { Id = fromId + 3 };
+            // object data = new object[] { tile1, tile2, tile3 };
+
+            var tile1 = new Tile(fromId + 1, "Tile-1", "http://localhost:3000/tileimage1", "http://localhost:3000/article1");
+            var tile2 = new Tile(fromId + 2, "Tile-2", "http://localhost:3000/tileimage2", "http://localhost:3000/article2");
+            var tile3 = new Tile(fromId + 3, "Tile-3", "http://localhost:3000/tileimage3", "http://localhost:3000/article3");
+
+            var data = new List<Tile>() { tile1, tile2, tile3 };
 
             return Json(data);
         }
