600f54f5fb0b91ab304889a184a9f4d0e20460dd
Davide Spano <davide.spano.x@gmail.com>
2018-05-12 12:32:59 +0100

    moved js for Home Index to its own file and module with namespace

--- CosmicMixer/wwwroot/js/home.indexPage.js
+++ CosmicMixer/wwwroot/js/home.indexPage.js
@@ -1,3 +1,62 @@
-﻿(function (home, $, undefined) {
+﻿(function (homeIndex, $, undefined) {
 
-})(window.home = window.home || {}, jQuery);
+    function renderTile(context) {
+        var rendered = App.templates.tile(context);
+        $("#container-teils").append(rendered);
+    }
+
+    function getTiles(fromId, numTeils) {
+        var getTiles = services.dataService.getTiles(fromId, numTeils);
+
+        setTimeout(function () {
+            getTiles.abort();
+            $(window)
+                .off("scroll.index.getTiles", ajaxScroll)
+                .on("scroll.index.getTiles", ajaxScroll);
+        }, 5000);
+
+        getTiles
+            .then(
+                function (tiles) {
+                    $.each(tiles, function (i, tile) {
+                        console.log(JSON.stringify(tile));
+                        renderTile(tile);
+                        $(window)
+                            .off("scroll.index.getTiles", ajaxScroll)
+                            .on("scroll.index.getTiles", ajaxScroll);
+                    });
+                },
+                function (jqXHR, textStatus, err) {
+                    console.log(err);
+                    console.log(textStatus);
+                    alert(JSON.stringify(jqXHR));
+                }
+            )
+            .always(function () {
+                // clean up!
+            });
+    }
+
+    function ajaxScroll(event) {
+        let $wnd = $(window);
+        let $doc = $(document);
+        let $containerTeils = $("#container-teils");
+
+        if ($wnd.scrollTop() >= $doc.height() - $wnd.height() - 10) {
+            $(window).off("scroll.getTiles", ajaxScroll);
+
+            var lastId = $containerTeils.find("div.teil:last-child").attr("data-id");
+            getTiles(parseInt(lastId) + 1, 9);
+        }
+    }
+
+    $(document).ready(function () {
+        getTiles(1, 9);
+        $(window).on("scroll.index.getTiles", ajaxScroll);
+    });
+
+    return {
+        renderTile: renderTile
+    };
+
+})(window.homeIndex = window.homeIndex || {}, jQuery);
