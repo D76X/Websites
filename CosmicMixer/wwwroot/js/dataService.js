7f692c667bd6f3582643ba5b1864925603f16311
Davide Spano <davide.spano.x@gmail.com>
2018-05-14 11:48:02 +0100

    chnaged dataSevice to define an AMD module with constructor function

--- CosmicMixer/wwwroot/js/dataService.js
+++ CosmicMixer/wwwroot/js/dataService.js
@@ -1,24 +1,19 @@
-﻿(function (services, $, undefined) {
+﻿define(['jquery'], function ($) {
 
-    var dataService = function () {
+    function DataService() {
 
         var urlBase = "http://localhost:3000/Home";
         var urlTiles = urlBase + "/GetTiles";
 
-        getTiles = function (fromId, numTeils) {
+        this.getTiles = function (fromId, numTeils) {
             return $.getJSON(
                 urlTiles, {
                     fromId: fromId,
                     numTeils: numTeils,
                 });
-        };
-
-        return {
-            getTiles: getTiles
-        };
+        };        
     }
 
-    services.dataService = dataService();
-
-})(window.services = window.services || {}, jQuery);
+    return new DataService();
+});
 
