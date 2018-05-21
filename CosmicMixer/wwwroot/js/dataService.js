d4c8573fc0f56942898c1985c794a68333cecdd0
Davide Spano <davide.spano.x@gmail.com>
2018-05-21 10:43:36 +0100

    removed logging statement

--- CosmicMixer/wwwroot/js/dataService.js
+++ CosmicMixer/wwwroot/js/dataService.js
@@ -7,9 +7,6 @@
         var getUrl = window.location;
         var baseUrl = getUrl.protocol + "//" + getUrl.host + getUrl.pathname.split('/')[1] + '/';
         var urlTiles = baseUrl + "Home/GetTiles";
-
-        console.log(urlTiles);
-
         this.getTiles = function (fromId, numTeils) {
             return $.getJSON(
                 urlTiles, {
